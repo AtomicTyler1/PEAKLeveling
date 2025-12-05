353702856a9ce5caa52b9205e4f6d529d9963393
Kirsho <mufuz328@gmail.com>
2025-12-05 22:29:46 +0100

    Add awarding Experience on Item use
    Experience gained is depending on rarity of the item.

---
+++ Leveling/Leveling/src/Leveling/Awarders/UseItemPatches.cs
@@ -0,0 +1,78 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Leveling.Awarders;
+
+internal class UseItemPatches
+{
+    private const Rarity FallbackRarity = Rarity.Common;
+
+    private static float CalculateExperience(Rarity itemRarity)
+    {
+        float weight = LootData.RarityWeights[itemRarity];
+
+        return 100 / weight * Mathf.Log10(weight);
+    }
+
+    private static bool TryGetItemRarity(GameObject itemObject, out Rarity itemRarity)
+    {
+        LootData itemData = itemObject.GetComponent<LootData>();
+        if (itemData == null)
+        {
+            // Default to common rarity if none is present
+            itemRarity = Rarity.Common;
+            return false;
+        }
+
+        itemRarity = itemData.Rarity;
+        return true;
+    }
+
+    [HarmonyPatch(typeof(Item), nameof(Item.FinishCastPrimary))]
+    [HarmonyPostfix]
+    public static void OnPrimaryUse(Item __instance)
+    {
+        if (!__instance.lastHolderCharacter.IsLocal || __instance.OnPrimaryFinishedCast == null)
+        {
+            return;
+        }
+
+        if (TryGetItemRarity(__instance.gameObject, out Rarity itemRarity))
+        {
+            itemRarity = FallbackRarity;
+        }
+
+        float usesFactor = 1;
+        if (__instance.totalUses > 0)
+        {
+            usesFactor = __instance.totalUses;
+        }
+
+        float expToGive = CalculateExperience(itemRarity) / usesFactor;
+        LevelingAPI.AddExperience(expToGive);
+    }
+
+    [HarmonyPatch(typeof(Item), nameof(Item.FinishCastSecondary))]
+    [HarmonyPostfix]
+    public static void OnSecondaryUse(Item __instance)
+    {
+        if (!__instance.lastHolderCharacter.IsLocal || __instance.OnSecondaryFinishedCast == null)
+        {
+            return;
+        }
+
+        if (TryGetItemRarity(__instance.gameObject, out Rarity itemRarity))
+        {
+            itemRarity = FallbackRarity;
+        }
+
+        float usesFactor = 1;
+        if (__instance.totalUses > 0)
+        {
+            usesFactor = __instance.totalUses;
+        }
+
+        float expToGive = CalculateExperience(itemRarity) / usesFactor;
+        LevelingAPI.AddExperience(expToGive);
+    }
+}
