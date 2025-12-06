3c91b23f6786419052d26d6853f766f6bc6f92fa
Kirsho <mufuz328@gmail.com>
2025-12-06 15:17:26 +0100

    Add hook to gain experience on achieving a badge

---
+++ Leveling/Leveling/src/Leveling/Awarders/AchievementPatches.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace Leveling.Awarders;
+
+[HarmonyPatch]
+internal class AchievementPatches
+{
+    private const float BadgeExp = 10f;
+
+    [HarmonyPatch(typeof(GlobalEvents), nameof(GlobalEvents.OnAchievementThrown))]
+    [HarmonyPostfix]
+    static void AwardExperienceForGettingAchievement()
+    {
+        LevelingAPI.AddExperience(BadgeExp);
+    }
+}
