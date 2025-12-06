c98c5ddea4f1b6626dd37c61fbf1298cb73f7031
atomictyler <tylercalladine11@gmail.com>
2025-12-06 20:13:28 +0000

    Added each segment giving 10xp base and +5xp added per segment

---
+++ Leveling/Leveling/src/Leveling/Awarders/CampfirePatches.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using Photon.Pun.Demo.PunBasics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leveling.Awarders;
+
+[HarmonyPatch]
+public class CampfirePatches
+{
+    private static float xpToAward = 10f;
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Campfire), nameof(Campfire.Light_Rpc))]
+    public static void Campfire_Light_Rpc_Patch(Campfire __instance)
+    {
+        float bonusXp = __instance.advanceToSegment switch
+        {
+            Segment.Tropics => 0f,
+            Segment.Alpine => 5f,
+            Segment.Caldera => 10f,
+            Segment.TheKiln => 15f,
+            _ => 0f
+        };
+
+        float totalXp = xpToAward + bonusXp;
+        LevelingAPI.AddExperience(totalXp);
+    }
+}
